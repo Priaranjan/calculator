d29d2a180bbbc6e3bd2246029702715f3f9a2f1b
Priaranjan <priaranjan53@gmail.com>
2025-01-03 22:24:36 +0530

    Add display and calculate functionalities

---
+++ main.js
@@ -0,0 +1,102 @@
+let operator='';
+let previousValue='';
+let currentValue='';
+document.addEventListener("DOMContentLoaded",function(){
+    //Store all HTML components on JS
+    let clear=document.querySelector('.clear');
+    let equal=document.querySelector('.equal');
+    let decimal=document.querySelector('.decimal');
+
+    let numbers=document.querySelectorAll('.number');
+    let operators=document.querySelectorAll('.operator');
+
+    let previousScreen=document.querySelector('.previous');
+    let currentScreen = document.querySelector('.current');
+
+    numbers.forEach((number)=>number.addEventListener("click",function(e){
+        handleNumber(e.target.textContent);
+        currentScreen.textContent=currentValue;
+    }))
+
+    operators.forEach((op)=>op.addEventListener("click",function(e){
+        handleOperator(e.target.textContent);
+        previousScreen.textContent=previousValue+" "+operator;
+        currentScreen.textContent=currentValue;
+    }))
+
+    clear.addEventListener("click",function(){
+        previousScreen.textContent="";
+        previousValue='';
+        currentScreen.textContent='';
+        currentValue='';
+        operator=''
+
+    })
+
+    equal.addEventListener("click",function(){
+        if(currentValue!='' && previousValue!=''){
+            calculate();
+            previousScreen.textContent=previousValue;
+            if(previousScreen.length<=5){
+                currentScreen.textContent=previousValue;
+
+            }
+            else{
+                currentValue.textContent=previousValue.slice(0,5)+"...";
+            }
+        }
+        
+    })
+
+    decimal.addEventListener("click",function(){
+        addDecimal();
+    })
+})
+
+
+function handleNumber(num){
+    if(currentValue.length<=5){
+        currentValue+=num;
+    }
+}
+
+function handleOperator(op){
+    operator=op;
+    previousValue=currentValue;
+    currentValue=''
+}
+
+
+function calculate(){
+    previousValue=Number(previousValue);
+    currentValue=Number(currentValue);
+
+    if(operator=="+"){
+        previousValue=previousValue+currentValue
+    }
+    else if(operator=="-"){
+        previousValue=previousValue-currentValue;
+    }
+
+    else if(operator=='x'){
+        previousValue=previousValue*currentValue;
+    }
+
+    else if(operator=='/'){
+        previousValue=previousValue/currentValue;
+    }
+
+    previousValue=roundNumber(previousValue);
+    previousValue=previousValue.toString();
+    currentValue=previousValue.toString();
+}
+
+function roundNumber(num){
+    return Math.round(num*1000)/1000;
+}
+
+function addDecimal(){
+    if(!currentValue.includes(".")){
+        currentValue+=".";
+    }
+}
